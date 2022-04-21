26526fe6f65989c321af6b25f55f164c7f46e78c
hyuraku <32809703+hyuraku@users.noreply.github.com>
2022-04-21 22:12:25 +0900

    add how to get an image fron online

--- main.go
+++ main.go
@@ -19,17 +19,30 @@ func main() {
 	}
 
 	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 
 	minetype := resp.Header.Get("Content-Type")
-	if strings.Contains(minetype, "json") {
+	if strings.Contains(minetype, "application/json") {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 		json.Json(body)
+	} else if strings.Contains(minetype, "image") {
+		f, err := os.Create("image.jpg")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, resp.Body)
+		fmt.Println("File image.jpg downlaod in current working directory")
 	} else {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 		fmt.Println(string(body))
 	}
-
 }
